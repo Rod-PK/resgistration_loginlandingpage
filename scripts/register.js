22567f97008dd5ad8e394c4d6c1409f86fa38ba5
Rodney Hinson <rodneyhinson22@gamil.com>
2024-09-17 13:51:17 +0000

    changes to javascript

--- scripts/register.js
+++ scripts/register.js
@@ -1,10 +1,22 @@
 // Select form by id
-const registerForm = document.getElementById('register-form');
+const registerForm = document.querySelector("#register-form");
+console.log(registerForm);
 // Handle form submit event
-registerForm.onsubmit = function() {
+registerForm.addEventListener("submit", function () {
     // Collect username
     // Collect email
     // Collect pssword
     // Save user information
     // Send confirmation email
+});
+
+// Age variable
+const age = 44;
+// Square of age
+function squareOfAge(age) {
+    const squaredAge = age ** 2;
+    return squaredAge;
 }
+
+const squaredAge = squareOfAge(45)
+console.log(squaredAge);
